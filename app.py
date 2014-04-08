de5d73d8c5292815267228b9089da04a72dbe627
Ben Firshman <ben@firshman.co.uk>
2014-04-08 14:21:21 +0100

    Add production logging and exception reporting

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from flask.ext.restful import reqparse
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask import jsonify
 import json
+import logging
 import os
+from raven.contrib.flask import Sentry
 
 app = Flask(__name__)
 
@@ -16,6 +18,16 @@ if os.environ.get('BASIC_AUTH_USERNAME'):
     app.config['BASIC_AUTH_FORCE'] = True
     basic_auth = BasicAuth(app)
 
+# Sentry exception reporting
+if 'SENTRY_DSN' in os.environ:
+    sentry = Sentry(app, dsn=os.environ['SENTRY_DSN'])
+
+# Logging
+@app.before_first_request
+def setup_logging():
+    if not app.debug:
+        app.logger.addHandler(logging.StreamHandler())
+        app.logger.setLevel(logging.INFO)
 
 api = restful.Api(app)
 if 'DATABASE_URL' in os.environ:
